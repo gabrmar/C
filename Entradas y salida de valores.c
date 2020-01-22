825f5b63c904cc8a758a317e41f118e0a8413a6f
Gabriel Martínez Llano <gmll920628@gmail.com>
2020-01-21 21:20:44 -0500

    Modificación del gitignore, inclusión de código sobre variables y constantes e inicio de código sonre entrada y salida de datos en C.

---
+++ Entradas y salida de valores.c
@@ -0,0 +1,16 @@
+// Como ingresar  datos en C y mostrarlos en consola
+#include<stdio.h>
+
+int main (){
+
+    /*Existen diferentes maneras de recibir valores de entrada. Algunas son para recibir valiores de un tipo de datoe specífico mientras
+    que otras son de propósito general*/
+
+    // Asignando un valor char
+    char a = getchar(); // pide un valor tipo caracter
+
+
+    printf("char agregado es:%c",a);
+
+
+}
