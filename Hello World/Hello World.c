e5af8bdfc0a2bf87b88712037452250981621cde
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-05-05 22:19:47 -0500

    Agregando carpetas para organizar códigos

---
+++ Hello World/Hello World.c
@@ -0,0 +1,9 @@
+#include<stdio.h> // Librería  de entrdas y salidas estándares en C 
+
+int main(){
+
+ printf("Hello World"); // Imprimiendo frase 
+
+return 0; // No es obligatorio, pero es una buena práctica  toda función, inluyendo el main(), entregue algo
+
+}
