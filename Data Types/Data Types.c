09cb67532594d01838b7b7e9055a51b1cb0badc9
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-05-07 20:56:46 -0500

    Agregando más claridad al código de tipo de datos y creación de código para hacer pruebas en C.

--- Data Types/Data Types.c
+++ Data Types/Data Types.c
@@ -8,28 +8,30 @@ int main (){
     double z = 2.1; // floating point number with double precision
     char hello = 'a';  // single character. It must be writen between single quote marks
     
-    printf("Printing variables...\n"); // it is necessary to use scape chracters to start a new line
+    printf("Printing variables...\n"); // it is necessary to use escape chracters to start a new line
 
-    /*Depending of the datatype, scape characters are also used as placeholders. See more information
-    at:https://en.wikipedia.org/wiki/Printf_format_string*/
+    /*Depending of the datatype, escape characters are also used as placeholders. See more information
+    at:https://en.wikipedia.org/wiki/Printf_format_string      */
 
-    printf("%d\n",x); // decimal variable expected
-    printf("%g\n",y); // double or float variable expected
-    printf("%g\n",z); 
-    printf("%c\n",hello); //char variable expected
+    printf("Decimal variable:%d\n",x); // decimal variable expected
+    printf("Float variable (long decimal floating point format):%f\n",y); // float or double variable expected and will be printed in a extended format 
+    printf("Double variable (short decimal floating point format):%g\n",z); // Also float or double varaible expected and will be printed in a brief format 
+    printf("Char variable:%c\n",hello); //char variable expected
 
     /* Addittionally, C offers a close relationship with hardware data. We can ask for the amount of
      bytes required to store each variable using the following instruction:*/
 
-     int size1 = sizeof(x);
-     int size2 = sizeof(y);
-     int size3 = sizeof(z);
-     int size4 = sizeof(hello);
+    int size1 = sizeof(x);
+    int size2 = sizeof(y);
+    int size3 = sizeof(z);
+    int size4 = sizeof(hello);
 
-     printf("Printing memory occupied by variables...\n");
+    printf("\n");
+    printf("Printing memory occupied by variables...\n");
+    printf("\n");
 
     printf("Int bytes:%ld\n",size1); /* decimal variable expected. You can use ld as escape chatacter if you
-    known beforehand the  number is a big one (also known as big format numbers)*/
+    known beforehand the number is a big one (also known as big format numbers)*/
     printf("Float bytes:%d\n",size2);
     printf("Double bytes:%d\n",size3);
     printf("Char bytes:%ld\n",size1); // this time there is no difference between %d abnd %ld
---
+++ Tests.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// This is a test
+
+int main (){
+
+double x = 1.23;
+printf("%f\n",x);
+printf("%g\n",x) ;
+return 0;
+
+}
+
