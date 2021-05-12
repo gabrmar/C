b53e18580146e2c2922254929735b9ec6bd4f27a
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-05-11 21:24:12 -0500

    Modificación del código para separar los distintos casos  entrada y salida de valores en diferentes códigos.

---
+++ Entrada y salida de valores/Entradas y salida de caracteres.c
@@ -0,0 +1,19 @@
+// Como ingresar  datos en C y mostrarlos en consola
+#include<stdio.h>
+
+int main (){
+
+    /*Existen diferentes maneras de recibir valores de entrada. Algunas son para recibir valiores de un tipo de dato specífico mientras
+    que otras son de propósito general*/
+
+    //Pidiendo valores de caracteres
+    printf("Entrada datos aqui:\n");
+
+    char a = getchar(); // pide un valor tipo caracter
+    /*Cuando se utiliza getchar() sólo se espera que el usuario agregue un único caracter. Lo demás lo va a ignorar*/
+    printf("Primer valor:");
+    putchar(a);
+    printf("\n");
+
+    return 0;
+}
