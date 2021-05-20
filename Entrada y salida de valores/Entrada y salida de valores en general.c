57ff7baa90049db8b7ac5d6aea4581478602f2f8
Gabriel Martínez Llano <gmll920628@gmail.com>
2021-05-19 19:57:58 -0500

    Agregando comentarios

--- Entrada y salida de valores/Entrada y salida de valores en general.c
+++ Entrada y salida de valores/Entrada y salida de valores en general.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // Librería de entradas y salidas
 
 int main() {
-    int a;
-    scanf("%d", &a);
-
-    printf("You entered: %d", a);
+    int a; 
+    printf("Agrege un numero por favor:");
+    scanf("%d", &a); /*scanf permite pedir datos al usuario y formatarlos de acuerdo a la necesidad. 
+    En este caso está pidiendo una vairable entera*/
+    printf("Valor agregado:%d", a); // imprimiendo valor
 
     return 0;
 }
